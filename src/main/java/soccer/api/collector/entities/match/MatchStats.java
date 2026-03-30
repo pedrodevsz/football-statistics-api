64b408ffdf0eb6592ed834292509d35764b6a7b7
pedrodesz <pedrodevs25@gmail.com>
2026-03-30 14:48:08 -0300

    feat: add match entities

---
+++ src/main/java/soccer/api/collector/entities/match/MatchStats.java
@@ -0,0 +1,31 @@
+package soccer.api.collector.entities.match;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "match_stats")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MatchStats {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "match_id", nullable = false, unique = true)
+    private Match match;
+
+    private Integer homeShots;
+    private Integer awayShots;
+    private Integer homeShotsOnTarget;
+    private Integer awayShotsOnTarget;
+    private Integer homeCorners;
+    private Integer awayCorners;
+    private Integer homeFouls;
+    private Integer awayFouls;
+}
