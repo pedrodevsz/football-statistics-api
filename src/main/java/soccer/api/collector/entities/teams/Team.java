c79b3c543cf55fe897c98dedfa06f99019ab9420
pedrodesz <pedrodevs25@gmail.com>
2026-04-01 08:33:24 -0300

    feat: add match and teams entities

---
+++ src/main/java/soccer/api/collector/entities/teams/Team.java
@@ -0,0 +1,31 @@
+package soccer.api.collector.entities.teams;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "team")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
