9a86553c7b1b816f7f62dfaa562c16af42736e79
pedrodesz <pedrodevs25@gmail.com>
2026-04-06 14:19:10 -0300

    feat: update configuration and dependencies for H2 database and dotenv support

--- src/main/java/soccer/api/collector/components/MatchUpdateScheduler.java
+++ src/main/java/soccer/api/collector/components/MatchUpdateScheduler.java
@@ -1,7 +1,5 @@
 package soccer.api.collector.components;
 
-import java.util.concurrent.TimeUnit;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
